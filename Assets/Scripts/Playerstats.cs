affd32c442588915a523bcd1e8a9235030613de8
Fabiana Perez <fabianalertora003@gmail.com>
2023-10-17 16:54:45 -0500

    Se agrego el movimiento y disparo de player

--- Assets/Scripts/Playerstats.cs
+++ Assets/Scripts/Playerstats.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Playerstats : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private float speed;
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    public GameObject bulletPrefab;
+
+    private float shootTimer;
+    private float shootDelayTime;
+
     void Start()
     {
         
@@ -13,6 +22,29 @@ public class Playerstats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        timer();
+        Shoot();
+    }
+   void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb2d.velocity = new Vector2(horizontal, vertical);
+ 
+   }
+    void timer()
+    {
+        shootTimer += Time.deltaTime;
+    }
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && shootTimer >= shootDelayTime)
+        {
+            GameObject obj = Instantiate(bulletPrefab);
+            obj.transform.position = transform.position;
+            shootTimer = 0;
+        }
     }
 }
