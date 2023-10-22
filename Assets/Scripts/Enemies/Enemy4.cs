3f8f8981808523c5ce55154dca8c060d498091c4
fabianaPerez24 <fabianalertora003@gmail.com>
2023-10-21 19:46:59 -0500

    Se terminaron de crear los enemigos y sus disparos

---
+++ Assets/Scripts/Enemies/Enemy4.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy4 : MonoBehaviour
+{
+    [SerializeField]
+    private int life;
+
+    public float timerBala;
+    public float maxtimerBala;
+
+    public GameObject enemybullet;
+
+
+    Vector2 InitialPosition;
+    private void Start()
+    {
+        InitialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position= Vector2.zero;
+        position.x = Mathf.Cos(Time.time);
+        position.y = Mathf.Sin(Time.time);
+        transform.position= InitialPosition+position;
+
+        EnemyShoot();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerBullet"))
+        {
+            life--;
+            if (life < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            life--;
+            if (life < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    void EnemyShoot()
+    {
+        timerBala += Time.deltaTime;
+        if (timerBala >= maxtimerBala)
+        {
+
+
+            timerBala = 0;
+
+            Vector2 dir = new Vector2(1, 1);
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject obj = Instantiate(enemybullet);
+                obj.transform.position = transform.position;
+                obj.GetComponent<Enemy3Bullet>().SetDirection(dir);
+                dir.x -= 1;
+                //dir.y *= -1;
+            }
+
+
+        }
+    }
+
+}
